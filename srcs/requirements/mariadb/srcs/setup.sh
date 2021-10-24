d1cb4179e2bf8e3c40f46171d418b085e914e31b
IssaMDOunejjaR <otaku6issam@gmail.com>
2021-10-24 16:16:06 +0100

    inception

--- srcs/requirements/mariadb/srcs/setup.sh
+++ srcs/requirements/mariadb/srcs/setup.sh
@@ -5,12 +5,12 @@ then
 	/etc/init.d/mariadb setup
 	service mariadb start
 
-	mysql -u root -e "CREATE USER 'iounejja'@'%' IDENTIFIED BY '123456';"
-	mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'iounejja'@'%';"
-	mysql -u root -e "CREATE DATABASE wordpress;"
-	mysql -u root -e "GRANT ALL ON wordpress.* TO 'iounejja'@'%';"
+	mysql -u root -e "CREATE USER '${MARIADB_USER}'@'%' IDENTIFIED BY '${MARIADB_USER_PASSORD}';"
+	mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO '${MARIADB_USER}'@'%';"
+	mysql -u root -e "CREATE DATABASE ${WORDPRESS_DATABASE_NAME};"
+	mysql -u root -e "GRANT ALL ON ${WORDPRESS_DATABASE_NAME}.* TO '${MARIADB_USER}'@'%';"
 	mysql -u root -e "FLUSH PRIVILEGES;"
-	mysql -u root -D wordpress < /tmp/wordpress.sql
+	mysql -u root -D $WORDPRESS_DATABASE_NAME < /tmp/wordpress.sql
 
 	service mariadb stop
 fi
