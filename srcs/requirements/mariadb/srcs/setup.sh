d642e25faa13c90cd84b11c08f7a4b8fcdc4612b
IssaMDOunejjaR <otaku6issam@gmail.com>
2021-10-24 18:46:25 +0100

    inception

--- srcs/requirements/mariadb/srcs/setup.sh
+++ srcs/requirements/mariadb/srcs/setup.sh
@@ -7,8 +7,11 @@ then
 
 	mysql -u root -e "CREATE USER '${MARIADB_USER}'@'%' IDENTIFIED BY '${MARIADB_USER_PASSORD}';"
 	mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO '${MARIADB_USER}'@'%';"
+	mysql -u root -e "CREATE USER '${MARIADB_USER}'@'localhost' IDENTIFIED BY '${MARIADB_USER_PASSORD}';"
+	mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO '${MARIADB_USER}'@'localhost';"
 	mysql -u root -e "CREATE DATABASE ${WORDPRESS_DATABASE_NAME};"
 	mysql -u root -e "GRANT ALL ON ${WORDPRESS_DATABASE_NAME}.* TO '${MARIADB_USER}'@'%';"
+	mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'root';"
 	mysql -u root -e "FLUSH PRIVILEGES;"
 	mysql -u root -D $WORDPRESS_DATABASE_NAME < /tmp/wordpress.sql
 
